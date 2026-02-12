5c70d4d5e970c39e12823c5797bbd272855c1d90
omar <omardeveloper9091@gmail.com>
2026-02-13 01:50:10 +0200

    work

--- lib/core/utils/toast_manager.dart
+++ lib/core/utils/toast_manager.dart
@@ -1,13 +1,27 @@
+import 'dart:ui';
+
 import 'package:fluttertoast/fluttertoast.dart';
 
-class ToastManger{
-  static showToast(String message,[Toast? toastLength]) {
-    toastLength = toastLength ?? Toast.LENGTH_LONG;
+class ToastManger {
+  static showToast(
+      String message, {
+        Toast? toastLength,
+        int timeInSecForIosWeb = 2,
+        double? fontSize,
+        String? fontAsset,
+        ToastGravity? gravity,
+        Color? backgroundColor,
+        Color? textColor,
+      }) {
     Fluttertoast.showToast(
       msg: message,
-      toastLength: toastLength,
-      gravity: ToastGravity.BOTTOM,
-      timeInSecForIosWeb: 2,
+      toastLength: toastLength ?? Toast.LENGTH_LONG,
+      timeInSecForIosWeb: timeInSecForIosWeb,
+      fontSize: fontSize ?? 16.0,
+      fontAsset: fontAsset,
+      gravity: gravity ?? ToastGravity.BOTTOM,
+      backgroundColor: backgroundColor,
+      textColor: textColor,
     );
   }
 }
